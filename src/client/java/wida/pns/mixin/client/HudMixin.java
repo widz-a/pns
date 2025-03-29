d7c4b1f276669380a7943dfc67f74490980df2f5
widz_a_ <itswithermc@gmail.com>
2025-03-30 06:25:56 +0700

    lol

--- src/client/java/wida/pns/mixin/client/HudMixin.java
+++ src/client/java/wida/pns/mixin/client/HudMixin.java
@@ -6,7 +6,6 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.RenderTickCounter;
-import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
